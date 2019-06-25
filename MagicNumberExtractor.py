f64657c6814edaa089b109fd7079535c57dcad95
Adhyayan Panwar <nullvoiddeath@gmail.com>
2019-06-25 13:11:22 +0530

    Added CSV Database

--- MagicNumberExtractor.py
+++ MagicNumberExtractor.py
@@ -2,12 +2,10 @@ import binascii
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument("-f","--file", metavar="filename", type=str, nargs=1, help="Path of the file")
+filename = parser.add_argument("-f", dest="filename", help="Path of file")
 args = parser.parse_args()
-
-with open(args.file[0], "rb") as binary_file:
+with open(args.filename, "rb") as binary_file:
     data = binary_file.read()
-    data = data[0:4]
-    data = binascii.hexlify(data)
-    data = data.decode('utf-8')
+    data = binascii.hexlify(data[0:4]).decode('utf-8').upper()
+    data = ' '.join([data[i:i+2] for i in range(0, len(data), 2)])
     print('[+] Magic number of the given file: ', data)
